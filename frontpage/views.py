86c9303db5dd312502fe6c77dc6c08c97d9489ec
anon anon <delgado.raymond@gmail.com>
2014-02-27 22:55:16 -0500

    started wiring up my views!

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 # from django.views import generic
 
-from frontpage.models import Title
+from frontpage.models import Title, Slider
 
 # Create your views here.
 
 
+def base_page(request):
+    context = {'placeholder': "PLACEHOLDER TEXT FROM VIEW TEMPLATE"}
+    return render(request, 'frontpage/base.html', context)
+
+
 def home_page(request):
-    return render(request, 'frontpage/home.html')
+    context = {'placeholder': "PLACEHOLDER TEXT FROM VIEW TEMPLATE"}
+    return render(request, 'frontpage/home.html', context)
